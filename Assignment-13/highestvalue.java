76eb65e57a1dcaffeb521d078de9d5302ed58850
Shahariar6172 <shahariaral36@gmail.com>
2025-12-10 15:06:56 +0600

    Refactor highestvalue.java for improved readability

--- Assignment-13/highestvalue.java
+++ Assignment-13/highestvalue.java
@@ -3,30 +3,33 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 public class highestvalue {
 public static void main(String[] args) {
-        try {
-            Scanner sc = new Scanner(new File("Input.txt"));
-            PrintWriter pw = new PrintWriter("Output.txt");
-            sc.useDelimiter(",\\s*");
-            while (sc.hasNextInt()) {
-             int num = sc.nextInt();
-             int highestDigit = findHighestDigit(num);
-             pw.print(highestDigit + ", ");
+    try {
+         Scanner sc = new Scanner(new File("Input.txt"));
+         PrintWriter pw = new PrintWriter("Output.txt");
+         sc.useDelimiter(",\\s*");
+         while (sc.hasNextInt()) {
+           int num = sc.nextInt();
+           int highestDigit = findHighestDigit(num);
+           pw.print(highestDigit + ", ");
+           System.out.print(highestDigit + ", ");
             }
             sc.close();
             pw.close();
-            System.out.println("Output written to Output.txt successfully.");
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
+            System.out.println("\n\nOutput written to Output.txt successfully.");
+        } 
+    catch (Exception e) {
+            System.out.println("Error: "+e);
         }
     }
+  
     public static int findHighestDigit(int num) {
-    int maxDigit=0;
-    while (num>0) {
-            int digit = num % 10;
-            if (digit > maxDigit) {
-                maxDigit = digit;
+       int maxDigit =0;
+        while (num >0) {
+        int digit = num % 10;
+        if (digit > maxDigit) {
+         maxDigit = digit;
             }
-            num = num/10;
+        num = num/10;
         }
         return maxDigit;
     }
